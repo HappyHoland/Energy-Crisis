b1378ed4c91ab4beaa14853f922aac86bc01f5c0
happyholand <josefelipehol@gmail.com>
2024-01-29 16:23:49 -0300

    comments added

--- energy_crysis.cpp
+++ energy_crysis.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
  
 using namespace std;
+
+//Josephus flavius algorithm
+int josephus (int num, int hop) {
+    int res = 0;
+
+    for (int i = 1; i <= num - 1; i++) {
+        if (i == 1) {
+            continue;
+        } else {
+            res = ((hop - 1)%i + 1 + res)%i;
+        }
+                
+    }
+
+    return res + 1;
+}
  
 int main() {
  
@@ -13,6 +29,7 @@ int main() {
         
         cin >> nRegions;
         
+        //End case and invalid cases check
         if (nRegions == 0) {
             break;
         } else if (nRegions < 13) {
@@ -20,25 +37,20 @@ int main() {
             continue;
         }
         
+        //Find first hop amount that ends in region 13 for current number of regions
         for (; finalRegion != 13;hop++) {
             finalRegion = 0;
             
+            //Skip hops that will eliminate 13 quickly
             if (12%hop == 0 && hop != 1) {
                 continue;
             }
-        
-            for (int i = 1; i <= nRegions - 1; i++) {
-                if (i == 1) {
-                    continue;
-                } else {
-                    finalRegion = ((hop - 1)%i + 1 + finalRegion)%i;
-                }
-                
-            }
-        
-            finalRegion += 2;
+
+            //Calculate last region with current hop and number of regions
+            finalRegion = josephus(nRegions-1, hop) + 1;
         }
         
+        //correct hop and send
         cout << hop-1 << endl;
         
     } while (nRegions != 0);
